aabf3754c9e331160c2c78efa0b438c4a2f9a2ed
udara <uvlakshan@gmail.com>
2024-03-31 01:53:55 +0530

    Addded UI modifications

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
+
           Expanded(
             child: Container(
               decoration: BoxDecoration(
@@ -77,12 +78,13 @@ class _MyHomePageState extends State<MyHomePage> {
             child: ElevatedButton(
               onPressed: _navigateToScanPage,
               style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 203, 192, 192),
                 textStyle: TextStyle(
                   fontWeight: FontWeight.bold, // Make text bold
-                  fontSize: 20, // Increase font size
+                  fontSize: 28, // Increase font size
                 ),
               ),
-              child: Text('Check QR Updates'),
+              child: Text('QR Scan'),
             ),
           ),
         ],
@@ -102,22 +104,85 @@ class ScanResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Scanned Result'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Scanned Message:', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text(result, style: TextStyle(fontSize: 18)),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-              },
-              child: Text('Go Back to First Page'),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/railway1.jpg', // Path to your background image
+              fit: BoxFit.cover, // Cover the entire screen
             ),
-          ],
-        ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 100),
+                Container(
+                  // padding: EdgeInsets.all(8),
+                  height: 40,
+                  width: 390,
+                  color: Color.fromARGB(255, 48, 150, 233),
+                  child: Center(
+                    child: Text(
+                      'Scanned Message',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ), // Add spacing
+                // Result Container
+                Container(
+                  padding: EdgeInsets.all(8),
+                  height: 330,
+                  width: 390,
+                  color: Color.fromARGB(255, 212, 217, 220),
+                  child: Center(
+                  child: OverflowBox(
+                    maxWidth: double.infinity,
+                    maxHeight: double.infinity,
+                    child: Text(
+                      result,
+                      textAlign: TextAlign.center, // Center-align the text
+                      style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 23, 23, 23)),
+                    ),
+                  ),
+                ),
+                ),
+                SizedBox(height: 220), // Add spacing
+                // Go Back Container
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.popUntil(context, ModalRoute.withName('/'));
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(
+                      Size(280, 50), // Adjust width and height as needed
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(255, 160, 194, 222),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.arrow_back, color: Colors.black),
+                      SizedBox(width: 5),
+                      Text(
+                        'Go Back',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+         ),
+        ],
       ),
     );
   }
